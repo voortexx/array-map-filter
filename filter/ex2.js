fa830aefef8d9fc72ce088b4b4fe7d58c03125b6
Fabien Raymond <voortexxx@gmail.com>
2018-10-01 16:37:03 +0200

    Ex2

--- filter/ex2.js
+++ filter/ex2.js
@@ -15,8 +15,8 @@ Exemple d'entrée:
 
  */
 
-function getMultiplesOf(numbers, n) {
-}
+
+const getMultiplesOf = (numbers, n) => numbers.filter( number => number % n == 0)
 
 // Ne pas modifier l'export
 module.exports = getMultiplesOf;
