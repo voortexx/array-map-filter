b893536031d6004803ed2d4a38d79bd44aec529d
Fabien Raymond <voortexxx@gmail.com>
2018-10-01 16:31:46 +0200

    Filter 1

--- filter/ex1.js
+++ filter/ex1.js
@@ -11,8 +11,8 @@ Sortie attendue:
 
  */
 
-function getPositiveNumbers(numbers) {
-}
+
+const getPositiveNumbers = (numbers) => numbers.filter(number => number >= 0)
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
